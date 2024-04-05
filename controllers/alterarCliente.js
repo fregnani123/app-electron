c81225f2f20e8df4ba86f5b0b970b383fa016be5
fregnani123 <fabianofregnani@hotmail.com>
2024-04-04 21:48:23 -0300

    tela alterar cliente

---
+++ controllers/alterarCliente.js
@@ -0,0 +1,131 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const clienteCadastrarInput = document.querySelector("#clienteNovo");
+    const filterCliente = document.querySelector("#cpfFake");
+    const RGInput = document.querySelector("#rgFake");
+    const nascimentoInput = document.querySelector("#nascimento");
+    const enderecoInput = document.querySelector("#endereco");
+    const numeroInput = document.querySelector("#numero");
+    const bairroInput = document.querySelector("#bairro");
+    const cidadeInput = document.querySelector("#cidade");
+    const ufInput = document.querySelector("#uf");
+    const foneInput = document.querySelector("#fone");
+    const emailInput = document.querySelector("#email");
+    const ocupacaoInput = document.querySelector("#ocupacao");
+    const spanAlert = document.querySelector('.alert');
+    const msgAlert = document.querySelector('.msg');
+    const buttonAlterar = document.querySelector('#cadastraCliente');
+    const buttonAlert = document.querySelector('#bottonAlert');
+
+
+   
+
+    function criaAlert(msg) {
+        spanAlert.classList.toggle('alertDisplay')
+        msgAlert.innerText = msg;
+    }
+
+    buttonAlert.addEventListener('click', criaAlert);
+
+    function limparInput() {
+        clienteCadastrarInput.value = '';
+        filterCliente = '';
+        RGInput.value = '';
+        nascimentoInput.value = '';
+        enderecoInput.value = '';
+        numeroInput.value = '';
+        bairroInput.value = '';
+        cidadeInput.value = '';
+        ufInput.value = '';
+        foneInput.value = '';
+        emailInput.value = '';
+        ocupacaoInput.value = '';
+    }
+
+
+    filterCliente.addEventListener('input', () => {
+        const cpfCliente = filterCliente.value;
+        filtrarInformacoes(cpfCliente)
+
+    })
+
+    function filtrarInformacoes(cpf) {
+        const urlCliente = 'http://204.216.187.179:3000/clientes';
+
+        fetch(urlCliente, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + btoa('Freg123:Freg_1308')
+            }
+        }
+        ).then(response => {
+            return response.json()
+        }).then(clienteEncontrado => {
+            const clienteImprimir = clienteEncontrado.find(cliente => cliente.cpf === cpf)
+            if (clienteImprimir) {
+                clienteCadastrarInput.value = clienteImprimir.clienteNome;
+                RGInput.value = clienteImprimir.rg;
+                nascimentoInput.value = new Date(clienteImprimir.nascimento).toLocaleDateString();
+                enderecoInput.value = clienteImprimir.endereco;
+                numeroInput.value = clienteImprimir.numero;
+                bairroInput.value = clienteImprimir.bairro;
+                cidadeInput.value = clienteImprimir.cidade;
+                ufInput.value = clienteImprimir.uf;
+                foneInput.value = clienteImprimir.fone;
+                emailInput.value = clienteImprimir.email;
+                ocupacaoInput.value = clienteImprimir.ocupacao;
+            }
+
+        }).catch(error => {
+            console.error(error)
+        })
+    }
+
+
+    buttonAlterar.addEventListener('click', updateCliente);
+
+
+    function updateCliente() {
+
+        const urlUpdate = `http://204.216.187.179:3000/updateProduto/${EANDigitado}`;
+
+        const clienteCadastrarInput1 = clienteCadastrarInput.value;
+        const RGInput1 = RGInput.value;
+        const nascimentoInpu1t = nascimentoInput.value;  
+        const enderecoInput1 = enderecoInput.value; 
+        const numeroInput1 = numeroInput.value; 
+        const bairroInput1 = bairroInput.value;  
+        const cidadeInput1 = cidadeInput.value; 
+        const foneInput1 = foneInput.value; 
+        const emailInput1 = emailInput.value;  
+        const ocupacaoInput1 = ocupacaoInput.value;  
+
+
+        const produto = {
+            
+        };
+
+
+        fetch(urlUpdate, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': 'Basic ' + btoa('Freg123:Freg_1308'),
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(produto)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao atualizar produto: ' + response.status);
+                } else {
+                    const msg = 'Produto atualizado com sucesso!'
+                    criaAlert(msg);
+                    limparInputs();
+                }
+
+            })
+            .catch(error => {
+                console.error('Erro ao atualizar produto:', error);
+            });
+    }
+
+})
--- controllers/informacoesClientes.js
+++ controllers/informacoesClientes.js
@@ -1,15 +1,26 @@
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    const filterCliente = document.getElementById('cpfFake')
+    const filterCliente = document.getElementById('cpfFake');
+    const nomeCliente = document.querySelector(".clienteNome1");
+    const rgCliente = document.querySelector(".rgFakeCliente1");
+    const DataNasc = document.querySelector(".nascimentoCliente1");
+    const enderecoCliente = document.querySelector(".enderecoCliente1");
+    const numeroCliente = document.querySelector(".numeroCliente1");
+    const bairroCliente = document.querySelector(".bairroCliente1");
+    const cidadeCliente= document.querySelector(".cidadeCliente1");
+    const ufCliente= document.querySelector(".UFCliente1");
+    const telefoneCliente= document.querySelector(".foneCliente1");
+    const emailCliente= document.querySelector(".emailCliente1");
+    const ocupacaoCliente= document.querySelector(".ocupacaoCliente1");
 
 
     filterCliente.addEventListener('input', () => {
         const cpfCliente = filterCliente.value;
         filtrarInformacoes(cpfCliente)
-       
+
     })
-   
+
     function filtrarInformacoes(cpf) {
         const urlCliente = 'http://204.216.187.179:3000/clientes';
 
@@ -24,9 +35,19 @@ document.addEventListener('DOMContentLoaded', function () {
         }).then(clienteEncontrado => {
             const clienteImprimir = clienteEncontrado.find(cliente => cliente.cpf === cpf)
             if (clienteImprimir) {
-                console.log(clienteImprimir)
+                nomeCliente.innerHTML = clienteImprimir.clienteNome;
+                rgCliente.innerHTML = clienteImprimir.rg;
+                DataNasc.innerHTML = new Date(clienteImprimir.nascimento).toLocaleDateString();
+                enderecoCliente.innerHTML = clienteImprimir.endereco;
+                numeroCliente.innerHTML = clienteImprimir.numero;
+                bairroCliente.innerHTML = clienteImprimir.bairro;
+                cidadeCliente.innerHTML = clienteImprimir.cidade;
+                ufCliente.innerHTML = clienteImprimir.uf; 
+                telefoneCliente.innerHTML = clienteImprimir.fone; 
+                emailCliente.innerHTML = clienteImprimir.email; 
+                ocupacaoCliente.innerHTML = clienteImprimir.ocupacao; 
             }
-            
+
         }).catch(error => {
             console.error(error)
         })
